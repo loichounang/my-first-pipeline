0ffc2fec1b1f4edb18f3435cb3e60e9d40f2f477
hounang <rdct.hounang.loic@gmail.com>
2026-01-05 15:44:21 +0100

    modification du fichier main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return jsonify(message="🚀 Pipeline CI/CD automatisée - ça marche !", status="success")
+    return jsonify(message="🎓 Pipeline CI/CD - Projet de Groupe", status="success")
 
 @app.route('/health')
 def health():
